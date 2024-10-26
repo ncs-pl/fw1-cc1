23b93f819cd49b2821fcb8cdc55e232cf5318347
Paul Nicolas <nicolas.paul1@etu.univ-orleans.fr>
2024-10-26 15:04:05 +0200

    Ajout de l'application collec_management

--- cc/settings.py
+++ cc/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'collec_management.apps.CollecManagementConfig',
 ]
 
 MIDDLEWARE = [
