ee90c46244c560e6f6d442ec99daf85f5ee85e96
Paul Nicolas <nicolas.paul1@etu.univ-orleans.fr>
2024-11-06 09:17:26 +0100

    Ajout d'une view /collection et la template

--- collec_management/urls.py
+++ collec_management/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('collection/<int:n>', views.collection, name='collection'),
 ]
--- collec_management/views.py
+++ collec_management/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from collec_management.models import Collec
 
 def about(request):
     return render(request, 'about.html')
+
+def collection(request, n) :
+    c = get_object_or_404(Collec, id=n)
+    return render(request, 'collection.html', { 'collection': c })
